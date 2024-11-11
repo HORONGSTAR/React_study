1e7c604bc16821ca96d209a321a2c51b501b15ea
“github <github>
2024-11-11 18:16:25 +0900

    -

--- Quiz_Practice/ch12/src/Quiz01/todoSlice.js
+++ Quiz_Practice/ch12/src/Quiz01/todoSlice.js
@@ -5,7 +5,7 @@ const todoSlice = createSlice({
    initialState: [],
    reducers: {
       addTodo: (state, action) => state.concat({ id: Date.now(), text: action.payload }),
-      removeTodo: (state, action) => state.filter((todo) => todo.id !== action.payload),
+      removeTodo: (state, action) => state.filter((todo) => todo.id !== action.payload), //return을 하지 않으면 그냥 배열 그 자체만 만들어지므로 반드시 리턴
    },
 })
 
